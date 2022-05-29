f2888d53ee01aa9cc7ffa6428ca890da7043422d
Syed Rahim Saqib <59372091+Jack8861@users.noreply.github.com>
2022-05-29 09:13:22 +0530

    The upload

---
+++ SongPlaylistApp/src/com/rahim/Song.java
@@ -0,0 +1,32 @@
+package com.rahim;
+
+public class Song {
+    String title;
+    double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public Song() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+}
+
